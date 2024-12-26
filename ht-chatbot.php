822136257052b881893427b83daab57c310391fe
Hunghtecom <hung.phan@htecom.vn>
2024-12-26 14:58:19 +0700

    Create table Books for plugin Chatbot

--- ht-chatbot.php
+++ ht-chatbot.php
@@ -25,6 +25,7 @@ register_deactivation_hook(__FILE__, 'ht_chatbot_deactivate');
 // Perform actions when activing plugin
 function ht_chatbot_activate() {
     chatbot_conversations_table(); 
+    create_books_table();
 }
 
 function ht_chatbot_deactivate() {
@@ -63,8 +64,8 @@ function ht_chatbox_render_html() { ?>
     <div id="chat-toggle" class="chat-toggle">
 		<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="30" height="30" fill="#FFFFFF">
 	        <path d="M12 0C5.373 0 0 4.943 0 11.048c0 3.448 1.552 6.49 4.059 8.55v3.694c0 .384.437.598.747.376l4.352-3.27c.938.236 1.927.365 2.842.365 6.627 0 12-4.943 12-11.048S18.627 0 12 0zm5.768 7.598l-3.16 5.094c-.18.292-.547.389-.843.218l-2.834-1.715a.5.5 0 0 0-.527 0l-4.501 2.722c-.4.242-.887-.232-.638-.646l3.16-5.093c.18-.292.547-.389.843-.218l2.834 1.715a.5.5 0 0 0 .527 0l4.501-2.722c.4-.242.887.232.638.646z"/>
-	    </svg>
-    </div>
+	    </svg>	
+    </div>	
 
     <div id="chatbot-message" class="chatbox">
     	<div class="chat-header">
@@ -122,6 +123,27 @@ function chatbot_conversations_table() {
         PRIMARY KEY (id),
         KEY session_id (session_id),
         KEY user_id (user_id)
+    ) $charset_collate;";
+
+    require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
+    dbDelta($sql);
+}
+
+// Create new table books
+function create_books_table() {
+    global $wpdb;
+    $table_name = $wpdb->prefix . 'books';
+    $charset_collate = $wpdb->get_charset_collate();
+
+    $sql = "CREATE TABLE $table_name (
+        id BIGINT(20) UNSIGNED NOT NULL AUTO_INCREMENT,
+        title VARCHAR(255) NOT NULL,
+        description TEXT NOT NULL,
+        category VARCHAR(255) NOT NULL,
+        keyword VARCHAR(255) NOT NULL,
+        publisherDate DATE NOT NULL,
+        publisherName VARCHAR(255) NOT NULL,
+        PRIMARY KEY (id)
     ) $charset_collate;";
 
     require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
